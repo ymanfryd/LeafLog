0c019eac9d9d990f4e12e355ef9d4ecd4fdff437
ymanfryd <ymanfryd@student.21-school.ru>
2026-09-21 08:50:56 +0300

    chore: bootstrap project structure and core dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {'@': './src'},
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+      },
+    ],
+    'react-native-worklets/plugin',
+  ],
 };
